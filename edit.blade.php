245386d798d6905fb1a6277cda1d9658199578dd
MohamedAbdelHalim1 <88633374+MohamedAbdelHalim1@users.noreply.github.com>
2021-12-01 02:12:27 +0200

    Add files via upload

--- edit.blade.php
+++ edit.blade.php
@@ -15,7 +15,7 @@
 <div class="container">
   <h2>Student Create</h2>
   <hr>
-  <form action="{{url('/store/')}}" method="post" enctype="multipart/form-data" class="needs-validation" novalidate>
+  <form action="{{url('/update/')}}" method="post" enctype="multipart/form-data" class="needs-validation" novalidate>
 
     
 
@@ -45,9 +45,11 @@
 @endif
 
 
+<input type="hidden"  name="id" value="{{$data->id}}">
+
     <div class="form-group">
       <label for="uname">Name:</label>
-      <input type="text" class="form-control" id="uname" placeholder="Enter username" name="name" value="<?php echo old("name") ?>"  required>
+      <input type="text" class="form-control" id="uname" placeholder="Enter username" name="name" value="{{$data->name}}"  required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
@@ -55,49 +57,49 @@
 
     <div class="form-group">
       <label for="uname">E-mail:</label>
-      <input type="email" class="form-control" id="uname" placeholder="Enter Email" name="email" value="<?php echo old("email") ?>" required>
+      <input type="email" class="form-control" id="uname" placeholder="Enter Email" name="email" value="{{$data->email}}" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
 
-    <div class="form-group">
+    <!-- <div class="form-group">
       <label for="uname">Password:</label>
       <input type="password" class="form-control" id="uname" placeholder="Enter password" name="password" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
-    </div>
+    </div> -->
 
 
     <div class="form-group">
       <label for="uname">linkedIn:</label>
-      <input type="url" class="form-control" id="uname" placeholder="Enter content" name="linkedIn" value="<?php echo old("url") ?>" required>
+      <input type="url" class="form-control" id="uname" placeholder="Enter content" name="linkedIn" value="{{$data->linkedIn}}" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
 
     <div class="form-group">
       <label for="uname">address:</label>
-      <input type="text" class="form-control" id="uname" placeholder="Enter address" name="address" value="<?php echo old("address") ?>" required>
+      <input type="text" class="form-control" id="uname" placeholder="Enter address" name="address" value="{{$data->address}}" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
 
 
    
-    <select name="gender" id="gender" class="form-control" required>
+    <!-- <select name="gender" id="gender" class="form-control" required>
       <option value="">Gender</option>
       <option value="male">Male</option>
       <option value="female" >Female</option>                                                         
-    </select> 
+    </select>  -->
 
-
+<!-- 
     <div class="form-group">
       <label for="uname">Photo:</label>
       <input type="file" class="form-control" id="uname" name="image" value="<?php echo old("image") ?>" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
-
+ -->
 
 
     <button type="submit" class="btn btn-primary" name="submit">Upload</button>
