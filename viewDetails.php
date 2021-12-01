191088da199af9dbe7f3a7a337ff75e9734dfa65
MohamedAbdelHalim1 <88633374+MohamedAbdelHalim1@users.noreply.github.com>
2021-12-01 02:11:42 +0200

    Add files via upload

--- viewDetails.php
+++ viewDetails.php
@@ -4,8 +4,63 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\student;
+
 class viewDetails extends Controller
 {
+
+
+    public function index(){
+        $data=student::get();
+
+        return view('index' , ["data"=>$data]);
+    }
+
+
+    public function edit($id){
+        $data = student::find($id);
+        return view('edit' , ["data"=>$data]);
+
+
+    }
+
+    public function update(Request $request){
+
+        $data =$this->validate($request , [
+            "id"      => "required",
+            "name"     => "required|string",
+            "email"    => "required|email", 
+            // "password" => "required|min:6",
+            "linkedIn" => "required|url",
+            "address" =>"required",
+            // "gender" => "required|in:male,female",
+            // "image" => "required|image|mimes:jpeg,jpg,png,gif"
+     
+            ] );
+            $op = student::where("id",$request->id)->update($data);
+            return redirect(url('/student'));
+
+    }
+
+
+
+
+    public function delete($id){   //this id that i sent with href in index
+        $op = student::where('id',$id)->delete();
+        if ($op) {
+            $msg = "deleted";
+        }else {
+            $msg = "Error";
+        }
+        return redirect('/student/');
+    }
+
+
+
+
+
+
+
 
         public function create(){
 
@@ -21,24 +76,22 @@ class viewDetails extends Controller
                     "name"     => "required|string",
                     "email"    => "required|email", 
                     "password" => "required|min:6",
-                    "url" => "required|url",
-                    "address" =>"required|size:10",
+                    "linkedIn" => "required|url",
+                    "address" =>"required",
                     "gender" => "required|in:male,female",
                     "image" => "required|image|mimes:jpeg,jpg,png,gif"
              
                     ] );
-                    $name = $request->name;
-                    $email = $request->email;
-                    $linkedin = $request->url;
-                    $address = $request->address;
-                    $gender= $request->gender;
                     $image = $request->image;
                     $new_name = rand().time().'.'.$image->getClientOriginalExtension();
                     $image->move(public_path('images'), $new_name);
-                    return back()
-                    ->with('success','Your Data Sent Successfully.')
-                    ->with('image',$new_name); 
-                                
+                    // return back()
+                    // ->with('success','Your Data Sent Successfully.')
+                    // ->with('image',$new_name); 
+                    $op = student::create($data);
+                    if ($op) {
+                        return redirect('/student/');
+                    }
 
                     
                 
